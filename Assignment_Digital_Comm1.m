ce62cfce360510cb511d3391925282782b4760b1
HackerGhost <ahmedws93@hotmail.com>
2016-03-17 21:49:07 +0200

    Add the under sampling in time and frequency domain.

--- Assignment_Digital_Comm1.m
+++ Assignment_Digital_Comm1.m
@@ -15,7 +15,9 @@ hold on ;
 
 %quantizing fixed point numeric 
 % for m bits
-n = 7;
+n = 3;
+% as n is the number of digits in the fraction part
+% m is the total number of digits of presentation
 m = 2*n+1 ;
 % quantize the function using the parameters
 % as n increase you can see the increase in the efficiency of quantization
@@ -25,7 +27,13 @@ Quantized_Signal = double(fi(Signal,1,m,n));
 plot(t,Quantized_Signal,'b');
 % u decode for decoding 
 % encoding 2*m level as y is input
+hold off ;
+
+figure ;
 digital_signal= uencode( Quantized_Signal , m );
+xlabel('Ts');
+ylabel('digital levels');
+plot(t,digital_signal,'.');
 
 % mean square quantization 
 % get the value of q at each point
@@ -44,7 +52,7 @@ MSE = sum(mean_square)/length(mean_square)
 oversampling = figure  ;
 t=0:0.001:1; % time signal
 y=2*cos(2*pi*5*t);
-[B,A] = BUTTER(3,1000/100000,'low'); % butter fly filter
+[B,A] = butter(3,1000/100000,'low'); % butter fly filter
 zero_added_signal=zeros(1,length(y)*10);
 for i=1:length(y)
 zero_added_signal(i*10)=y(i);
@@ -55,17 +63,17 @@ zero_added_signal(1:9)=[];
 t=linspace(0,1,length(zero_added_signal));
 filtered_signal = filter(B,A,zero_added_signal);
 plot(t,filtered_signal,'r');
-XLABEL('time');
-YLABEL('oversampled signals');
+xlabel('time');
+ylabel('oversampled signals');
 
 
 %-----------------------% construction from minimum sampling--------
-figure
+figure;
 % minimum sampling is nyquios frequency = fs = 2*fmax = 10 
 t=0:0.1:1;    % replace ?? with the suitable number
 y=2*cos(2*pi*5*t);
-%
-[B,A] = BUTTER(10,0.1,'low');
+% fs = 10 nyquios = 2*fmax = 2*5 ;
+[B,A] = butter(10,0.1,'low');
 zero_added_signal=zeros(1,length(y)*10);
 for i=1:length(y)
 zero_added_signal(i*10)=y(i);
@@ -79,8 +87,36 @@ YLABEL('minimum sampled signals')
 s=fft(filtered_signal);
 s=fftshift(s);
 fs=100; % why 100?? Write your comments in the m file
+% because i added the zeros to the signal as it made 100 sample in the
+% second instead of the past fs as 10 
+%(Ts now is 1/100 for the main samples to have 1/10 distance
+%as this distance must be constant)
 freq=linspace(-fs/2,fs/2,length(s));
-figure
+figure;
 plot(freq,abs(s))
-XLABEL('freq')
-YLABEL('magnitude of minimum sampled signals')
+xlabel('freq')
+ylabel('magnitude of minimum sampled signals')
+
+%----------------------under sampling-------------------------------%
+
+
+figure;
+t=0:0.2:1;
+y=2*cos(2*pi*5*t);
+% the 2nd argument is the cuttoff frequency of the butterfly filter
+[B,A] = butter(10,0.2,'low');   
+t=linspace(0,1,length(y));
+filtered_signal = filter(B,A,y);
+plot(t,filtered_signal,'r');
+xlabel('time')
+ylabel('under sampled signals')
+s=fft(filtered_signal);
+s=fftshift(s);
+fs = 10 ;
+freq=linspace(-fs/2,fs/2,length(s));
+figure;
+plot(freq,abs(s))
+xlabel('freq axis')
+ylabel('magnitude of under sampled signals')
+
+%----------------------------------------------------------------%
