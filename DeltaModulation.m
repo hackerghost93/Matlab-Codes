dbf7e8a30ea64e5d026c2a94c055d7d304b3f7e2
HackerGhost <ahmedws93@hotmail.com>
2016-04-14 21:22:47 +0200

    Add some difference and plots to comparison.

--- DeltaModulation.m
+++ DeltaModulation.m
@@ -1,17 +1,30 @@
 function [deltaSignal] = DeltaModulation( t , y, Ts ,deltaAmp)
     %Assume initial Start at 0
     deltaSignal = zeros(1,length(y));
+    deltas = zeros(1,length(y));
     for i = 2:length(y)-1
         if(y(i) - deltaSignal(i-1) > 0)
             deltaSignal(i) = deltaSignal(i-1)+deltaAmp ;
+            deltas(i) = 1 ;
         else
             deltaSignal(i) = deltaSignal(i-1)-deltaAmp;
+            deltas(i) = 0 ;
         end 
     end 
     
     figure ;
+    subplot(2,1,1);
     plot(t,y,'red');
     hold on ;
     stairs(t,deltaSignal);
+    title('Input vs Output signal');
+    hold off ;
+    subplot(2,1,2);
+    stairs(t,deltas);
+    ylim([-1 2])
+    grid on ;
+    title('deltaModulated signal');
+    xlabel('time each step is Ts');
+    ylabel('binary Symbol NRZ');
 end
 
--- DeltaModulationAssignment.m
+++ DeltaModulationAssignment.m
@@ -8,6 +8,7 @@ deltaSignal = DeltaModulation(t,y,1/Fs,0.05);
 filtered = filter(b,a,deltaSignal);
 figure ;
 plot(t,filtered);
+title('filtered signal Sinusoidal wave ');
 RootMeanSquare(y,deltaSignal)
 
 %%%%%%%%%%%%%%%%%%%%%%%
@@ -17,19 +18,100 @@ deltaSignal = DeltaModulation(t,y,1/Fs,0.05);
 %butter take the degree and normalized cutoff frequency
 [b a] = butter(2,0.3,'low');
 filter(b,a,deltaSignal);
-%figure ;
-%plot(t,filtered);
+figure ;
+plot(t,filtered);
+title('filtered signal Unit Step');
 RootMeanSquare(y,deltaSignal)
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 %square wave
 y = square(2*pi*500*t);
-plot(t,y);
 deltaSignal = DeltaModulation(t,y,1/Fs,0.05);
 %butter take the degree and normalized cutoff frequency
 [b a] = butter(2,0.3,'low');
 filter(b,a,deltaSignal);
-%figure ;
-%plot(t,filtered);
+figure ;
+plot(t,filtered);
+title('filtered signal Square Wave');
+RootMeanSquare(y,deltaSignal)
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% delta = 0.1 delta %%%%%%%%%%%%%%%%%%
+Fs = 50000 ;
+t = 0:1/Fs:3/500;
+y = sin(2*pi*500*t);
+Wn = 2*pi*500/50000;
+deltaSignal = DeltaModulation(t,y,1/Fs,0.05*0.1);
+%butter take the degree and normalized cutoff frequency
+[b a] = butter(2,0.3,'low');
+filtered = filter(b,a,deltaSignal);
+figure ;
+plot(t,filtered);
+title('filtered signal * 0.1 delta Sinusoidal Wave');
+RootMeanSquare(y,deltaSignal)
+
+%%%%%%%%%%%%%%%%%%%%%%%
+%unit step
+y = ones(1,length(t));
+deltaSignal = DeltaModulation(t,y,1/Fs,0.05*0.1);
+%butter take the degree and normalized cutoff frequency
+[b a] = butter(2,0.3,'low');
+filter(b,a,deltaSignal);
+figure ;
+plot(t,filtered);
+title('filtered signal * 0.1 delta Unit Step');
+RootMeanSquare(y,deltaSignal)
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%square wave
+y = square(2*pi*500*t);
+deltaSignal = DeltaModulation(t,y,1/Fs,0.05*0.1);
+%butter take the degree and normalized cutoff frequency
+[b a] = butter(2,0.3,'low');
+filter(b,a,deltaSignal);
+figure ;
+plot(t,filtered);
+title('filtered signal * 0.1 delta Square wave');
+RootMeanSquare(y,deltaSignal)
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%0.1 Ts %%%%%%%%%%%%%%%%%%%%%%
+Fs = 50000*0.1 ;
+t = 0:1/Fs:3/500;
+y = sin(2*pi*500*t);
+Wn = 2*pi*500/50000;
+deltaSignal = DeltaModulation(t,y,1/Fs,0.05);
+%butter take the degree and normalized cutoff frequency
+[b a] = butter(4,0.5,'low');
+filtered = filter(b,a,deltaSignal);
+figure ;
+plot(t,filtered);
+title('filtered signal 0.1 Ts Sinusoidal wave');
+RootMeanSquare(y,deltaSignal)
+
+%%%%%%%%%%%%%%%%%%%%%%%
+%unit step
+y = ones(1,length(t));
+deltaSignal = DeltaModulation(t,y,1/Fs,0.05);
+%butter take the degree and normalized cutoff frequency
+[b a] = butter(4,0.5,'low');
+filter(b,a,deltaSignal);
+figure ;
+plot(t,filtered);
+title('filtered signal 0.1 Ts Unit Step');
+RootMeanSquare(y,deltaSignal)
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%square wave
+y = square(2*pi*500*t);
+deltaSignal = DeltaModulation(t,y,1/Fs,0.05);
+%butter take the degree and normalized cutoff frequency
+[b a] = butter(4,0.5,'low');
+filter(b,a,deltaSignal);
+figure ;
+plot(t,filtered);
+title('filtered signal 0.1 Ts Square Wave');
 RootMeanSquare(y,deltaSignal)
