beabfd6e272ae67c7f3daa1e281e1020a5f9bc84
HassanRezk <hassan.rms93@gmail.com>
2016-04-29 16:58:23 +0300

    Add second case and screen shots.

--- Gram shmidt/BasisFunctionAssignment.m
+++ Gram shmidt/BasisFunctionAssignment.m
@@ -24,3 +24,21 @@ xlabel('first basis')
 ylabel('2nd basis')
 zlabel('third basis')
 
+
+y_total = [y2;y1;y3;y4] ;
+[time , basis_total , basisNumber] = BasisGenerator(t,y_total);
+%DrawConstellation(basis_total , y_total);
+
+signalNumber = 4;
+points = zeros(signalNumber,basisNumber);
+for i = 1 : signalNumber
+    for j = 1 : basisNumber 
+        points(i,j) = dot(y_total(i,:),basis_total(j,:));
+    end
+end
+figure;
+%points 
+scatter3(points(:,1),points(:,2),points(:,3),'filled');
+xlabel('first basis')
+ylabel('2nd basis')
+zlabel('third basis')
